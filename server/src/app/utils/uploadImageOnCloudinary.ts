fe636a2fa60e5508deac14c9ebf13aad0bf42b01
Ahmdpolash <ahmedpolash732@gmail.com>
2025-03-15 16:40:37 +0600

    Setting Up Cloudinary for Image Upload

--- server/src/app/utils/uploadImageOnCloudinary.ts
+++ server/src/app/utils/uploadImageOnCloudinary.ts
@@ -1,7 +1,17 @@
 import multer from "multer";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import config from "../../config";
 
-export const storage = multer.diskStorage({
+// cloudinary Configuration
+cloudinary.config({
+  cloud_name: config.cloudinary.cloudinary_cloud_name,
+  api_key: config.cloudinary.cloudinary_api_key,
+  api_secret: config.cloudinary.cloudinary_api_secret,
+});
+
+// multer configuration
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(process.cwd(), "uploads"));
   },
@@ -12,6 +22,19 @@ export const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+// upload image on cloudinary
+const uploadToCloudinary = async (imageName: string, path: string) => {
+  const uploadResult = await cloudinary.uploader
+    .upload(path, {
+      public_id: imageName,
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+};
 
 export const fileUploader = {
   upload,
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -21,4 +21,10 @@ export default {
     app_password: process.env.APP_PASSWORD,
     host: process.env.HOST,
   },
+
+  cloudinary: {
+    cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+    cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+  },
 };

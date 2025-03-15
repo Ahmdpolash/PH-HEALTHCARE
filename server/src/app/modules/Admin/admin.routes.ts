1c773675c32b1fb3affa0bb634aa62bc7814aafe
Ahmdpolash <ahmedpolash732@gmail.com>
2025-03-15 11:21:32 +0600

    Implementing Role-Based Access Control on API Routes

--- server/src/app/modules/Admin/admin.routes.ts
+++ server/src/app/modules/Admin/admin.routes.ts
@@ -2,23 +2,45 @@ import { Router } from "express";
 import { AdminControllers } from "./admin.controller";
 import validateRequest from "../../middleware/validateRequest";
 import { adminValidationSchemas } from "./admin.validatons";
+import { auth } from "../../middleware/auth";
+import { UserRole } from "@prisma/client";
 
 const router = Router();
 
-router.get("/", AdminControllers.getAllAdmin);
+router.get(
+  "/",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  AdminControllers.getAllAdmin
+);
 
-router.get("/:id", AdminControllers.getByIdFromDb);
+router.get(
+  "/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
 
-router.delete("/:id", AdminControllers.deleteFromDb);
+  AdminControllers.getByIdFromDb
+);
+
+router.delete(
+  "/:id",
+
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+
+  AdminControllers.deleteFromDb
+);
 
 router.patch(
   "/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+
   validateRequest(adminValidationSchemas.update),
   AdminControllers.updateIntoDb
 );
 
-router.delete("/:id", AdminControllers.deleteFromDb);
+router.delete(
+  "/soft/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
 
-router.delete("/soft/:id", AdminControllers.softDeleteFromDB);
+  AdminControllers.softDeleteFromDB
+);
 
 export const AdminRoutes = router;
--- server/src/app/modules/User/user.route.ts
+++ server/src/app/modules/User/user.route.ts
@@ -2,10 +2,11 @@ import { Request, Response, Router } from "express";
 import { userControllers } from "./user.controllers";
 import { AdminControllers } from "../Admin/admin.controller";
 import { auth } from "../../middleware/auth";
+import { UserRole } from "@prisma/client";
 
 const router = Router();
 
-router.post("/create-admin", auth("ADMIN"), userControllers.createAdmin);
+router.post("/create-admin", auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), userControllers.createAdmin);
 
 router.get("/admin", AdminControllers.getAllAdmin);
 

4410109bf6055afd618ddc5d3ba2b3f1badc7ecf
Ahmdpolash <ahmedpolash732@gmail.com>
2025-03-28 22:46:17 +0600

    schedula slot system api added

--- server/src/app/modules/Schedule/schedule.service.ts
+++ server/src/app/modules/Schedule/schedule.service.ts
@@ -1,12 +1,17 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
+import ApiError from "../../error/ApiError";
 
 const inserIntoDB = async (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
 
+  const interverlTime = 30;
+  const schedules = [];
+
   const currentDate = new Date(startDate);
   const lastDate = new Date(endDate);
 
-  while (currentDate >= startDate) {
+  while (currentDate <= lastDate) {
     const startDateTime = new Date(
       addHours(
         `${format(currentDate, "yyyy-MM-dd")}`,
@@ -16,15 +21,40 @@ const inserIntoDB = async (payload: any) => {
 
     const endDateTime = new Date(
       addHours(
-        `${format(lastDate, "yyyy-MM-dd")}`,
+        `${format(currentDate, "yyyy-MM-dd")}`,
         Number(endTime.split(":")[0])
       )
     );
 
-    while (startDateTime <= endDateTime) {
-      // next module.
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, interverlTime),
+      };
+
+      const existingSchedule = await prisma.schedule.findFirst({
+        where: {
+          startDateTime: scheduleData.startDateTime,
+          endDateTime: scheduleData.endDateTime,
+        },
+      });
+
+      if (existingSchedule) {
+        throw new ApiError(400, "schedule already exists");
+      } else {
+        const result = await prisma.schedule.create({
+          data: scheduleData,
+        });
+        schedules.push(result);
+      }
+
+      startDateTime.setMinutes(startDateTime.getMinutes() + interverlTime);
     }
+
+    currentDate.setDate(currentDate.getDate() + 1);
   }
+
+  return schedules;
 };
 
 export const ScheduleService = {

1bb3057ab26e9ecf135c0637f0b833cf7fe66ec0
Ahmdpolash <ahmedpolash732@gmail.com>
2025-03-29 11:43:06 +0600

    Retrieving Doctor Schedule with Filtering

--- server/src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
+++ server/src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -1,26 +1,55 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import { catchAsync } from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sentResponse";
 import { IAuthUser } from "../../interface/common";
-import httpStatus from 'http-status'
+import httpStatus from "http-status";
 import { DoctorScheduleService } from "./doctorSchedule.service";
+import { IPaginationOptions } from "../../interface/pagination";
+import { paginationHelper } from "../../../helper/paginationHelper";
+import prisma from "../../../shared/prisma";
+import { Prisma } from "@prisma/client";
+import pick from "../../../shared/pick";
+import { scheduleFilterableFields } from "./doctorSchedule.constant";
 
-
-const insertIntoDB = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
-
+const insertIntoDB = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
     const user = req.user;
 
     const result = await DoctorScheduleService.insertIntoDB(user, req.body);
 
     sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Doctor Schedule created successfully!",
-        data: result
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Doctor Schedule created successfully!",
+      data: result,
     });
-});
+  }
+);
+
+// get doctor schedule
+
+const getMySchedule: RequestHandler = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const filters = pick(req.query, scheduleFilterableFields);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+    const user = req.user;
+    const result = await DoctorScheduleService.getMySchedule(
+      filters,
+      options,
+      user
+    );
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "My Schedule retrieved successfully",
+      data: result,
+    });
+  }
+);
 
 export const DoctorScheduleController = {
-    insertIntoDB,
-   
+  insertIntoDB,
+  getMySchedule,
 };

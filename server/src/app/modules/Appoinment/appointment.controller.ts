916435b9a82fafaf4c2ab42b0dfa653df878048d
Ahmdpolash <ahmedpolash732@gmail.com>
2025-04-20 15:39:32 +0600

    all appointment data get and appoinment validation added

--- server/src/app/modules/Appoinment/appointment.controller.ts
+++ server/src/app/modules/Appoinment/appointment.controller.ts
@@ -4,6 +4,7 @@ import sendResponse from "../../../shared/sentResponse";
 import httpStatus from "http-status";
 import { AppointmentServices } from "./appointment.services";
 import pick from "../../../shared/pick";
+import { appointmentFilterableFields } from "./appointment.constant";
 
 const createAppointment: RequestHandler = catchAsync(async (req, res) => {
   const user = req.user;
@@ -31,7 +32,20 @@ const getMyAppointment: RequestHandler = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: "Appointment retrived successfully!",
+    message: "My Appointment retrived successfully!",
+    data: result,
+  });
+});
+
+const getAllAppointment: RequestHandler = catchAsync(async (req, res) => {
+  const filters = pick(req.query, appointmentFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await AppointmentServices.getAllAppointment(filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "All Appointment retrived successfully!",
     data: result,
   });
 });
@@ -39,4 +53,5 @@ const getMyAppointment: RequestHandler = catchAsync(async (req, res) => {
 export const AppointmentControllers = {
   createAppointment,
   getMyAppointment,
+  getAllAppointment,
 };
--- server/src/app/modules/Appoinment/appointment.route.ts
+++ server/src/app/modules/Appoinment/appointment.route.ts
@@ -2,13 +2,22 @@ import { Router } from "express";
 import { AppointmentControllers } from "./appointment.controller";
 import { auth } from "../../middleware/auth";
 import { UserRole } from "@prisma/client";
+import validateRequest from "../../middleware/validateRequest";
+import { AppointmentValidation } from "./appointment.validation";
 
 const router = Router();
+
+// get all appointment route
+router.get(
+  "/",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  AppointmentControllers.getAllAppointment
+);
 
 // get my appointment route
 router.get(
   "/my-appointment",
-  auth(UserRole.PATIENT,UserRole.DOCTOR),
+  auth(UserRole.PATIENT, UserRole.DOCTOR),
   AppointmentControllers.getMyAppointment
 );
 
@@ -16,6 +25,7 @@ router.get(
 router.post(
   "/",
   auth(UserRole.PATIENT),
+  validateRequest(AppointmentValidation.createAppointment),
   AppointmentControllers.createAppointment
 );
 

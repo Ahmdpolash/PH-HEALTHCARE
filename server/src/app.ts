1a3957f55500b78e5f34352e38c9682e5541d21e
Ahmdpolash <ahmedpolash732@gmail.com>
2025-04-23 12:46:00 +0600

    update the global error handler code

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,6 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// delete unpaid payment
 cron.schedule("* * * * *", () => {
   try {
     AppointmentServices.cancelUnpaidAppointment();

5704b7a6dfdf659f226cda82093ec0b574e483a7
Ahmdpolash <ahmedpolash732@gmail.com>
2025-04-24 15:37:39 +0600

    added image configuration on next config

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@mui/material";
 
-
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
       <Button>Text</Button>
-   </div>
-  )
+    </div>
+  );
 }

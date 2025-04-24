97afd360a810d297be03d30be25a4b890ca5b9e9
Ahmdpolash <ahmedpolash732@gmail.com>
2025-04-24 12:24:42 +0600

    frontend setup done with MUI

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Button variant="contained">Text</Button>
+   </div>
+  )
+}

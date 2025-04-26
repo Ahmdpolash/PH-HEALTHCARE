d88051550b56cc7385358b4adc5a262f2837f05a
Ahmdpolash <ahmedpolash732@gmail.com>
2025-04-26 22:16:56 +0600

    add stats , how works section on homepage

--- client/src/app/(commonLayout)/page.tsx
+++ client/src/app/(commonLayout)/page.tsx
@@ -1,7 +1,9 @@
 import BuyMedicine from "@/components/ui/home/BuyMedicine";
 import Hero from "@/components/ui/home/Hero";
+import HowItWorks from "@/components/ui/home/HowItWorks";
 import Partner from "@/components/ui/home/Partner";
 import Specialist from "@/components/ui/home/Specialist";
+import Stats from "@/components/ui/home/Stats";
 import TopRatedDoctors from "@/components/ui/home/TopRatedDoctors";
 import WhyChoose from "@/components/ui/home/WhyChoose";
 
@@ -13,8 +15,10 @@ export default function Home() {
       <Specialist />
       <TopRatedDoctors />
       <WhyChoose />
+      <HowItWorks />
       <BuyMedicine />
       <Partner />
+      <Stats />
     </div>
   );
 }

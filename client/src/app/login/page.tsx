4593393f24fff0b6fd38ea288b2703da45ab285f
Ahmdpolash <ahmedpolash732@gmail.com>
2025-04-26 08:41:50 +0600

    login page ui done

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,8 +1,103 @@
+import assets from "@/assets";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
 const Login = () => {
   return (
-    <div>
-      <p> Hello, This is login Page </p>
-    </div>
+    <Container>
+      <Stack
+        sx={{
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            padding: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Login
+              </Typography>
+            </Box>
+          </Stack>
+
+          <Box mt={2}>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    // {...Login("patient.email")}
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    label="Password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    // {...Login("patient.password")}
+                  />
+                </Grid>
+              </Grid>{" "}
+              <Link
+                href="/forgot-password"
+                className="text-right cursor-pointer grid place-content-right"
+              >
+                Forgot Password?
+              </Link>
+              <Button
+                sx={{
+                  margin: "13px 0px",
+                }}
+                fullWidth={true}
+                type="submit"
+              >
+                Login
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Don&apos;t have an account ?
+                <Link href="/register" className="no-underline">
+                  {" "}
+                  Create An Account
+                </Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
+
 export default Login;
